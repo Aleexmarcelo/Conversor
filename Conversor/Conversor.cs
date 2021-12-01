93e91772bf6ed39237e723123a14277c8843eadb
Aleexmarcelo <jojoanete@gmail.com>
2021-12-01 19:08:03 -0300

    Adicionar arquivos de projeto.

---
+++ Conversor/Conversor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conversor
+{
+    class Conversor
+    {
+        //Criada as variáveis
+        private double ReaisTotal;
+        private double TaxaTotal;
+        public double IOF = 0.0408;
+        private double Taxa;
+        //Encapsulamento
+        public double DolarAtual { get; set; }
+        public double QntDolar { get; set; }
+        //Construtor
+        public Conversor(double dolarAtual, double qntDolar)
+        {
+            DolarAtual = dolarAtual;
+            QntDolar = qntDolar;
+        }
+        //Método de conversão sem as taxas
+        public void Conversao()
+        {
+            ReaisTotal = DolarAtual * QntDolar;
+        }
+        ////Método de conversão com as taxas
+        public void ConversaoTaxa()
+        {
+            TaxaTotal = Taxa + ReaisTotal;
+            Taxa = (ReaisTotal * IOF);
+        }
+        //Método ToStrig usand override
+        public override string ToString()
+        {
+            //Mostrando o "recibo" em console com todos os dados
+            return "Valor total em reais sem IOF = " + ReaisTotal.ToString("C2") + "\nValor do imposto = " + Taxa.ToString("C2") + "(IOF:4,08%) " + "\nValor total com IOF incluso = " + TaxaTotal.ToString("C2");
+        }
+    }
+}
---
+++ Conversor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+namespace Conversor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            //Pedindo informações sobre o valor do dólar
+            Console.WriteLine("Quanto está o dólar hoje?");
+            //Lendo o que foi digitado e salvo em variável
+            double DolarAtual = double.Parse(Console.ReadLine());
+            //Pedindo a quantidade em dólares
+            Console.WriteLine("\nQuantidade de dólar a ser comprado?");
+            //Lendo o que foi digitado e salvo em variável
+            double QntDolar = double.Parse(Console.ReadLine());
+            //Instancia um conversor com as variáveis
+            Conversor conversor = new Conversor(DolarAtual, QntDolar);
+            //Executando os métodos sem taxa e com taxa
+            conversor.Conversao();
+            conversor.ConversaoTaxa();
+            //Escrevendo todos os dados coletados
+            Console.WriteLine(conversor.ToString());
+        }
+    }
+}
+
+//Link da página onde foi cotado o valor do dólar(R$5,56 com IOF de 2,89% ):https://g1.globo.com/economia/noticia/2021/11/24/dolar.ghtml
